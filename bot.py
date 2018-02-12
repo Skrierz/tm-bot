d05920b98fd5c0c4ebcb8e82f5e56333ba314f16
Skrierz <sleipknir@gmail.com>
2018-02-12 17:47:50 +0300

    123

--- bot.py
+++ bot.py
@@ -1,55 +1,20 @@
-# import logging
-# import telegram
-# from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+import telegram
+
+bot = telegram.Bot(token='')
+
+# chat_id = bot.get_updates()[-1].message.chat_id
+
+# bot.send_chat_action(chat_id=chat_id, action=telegram.ChatAction.TYPING)
 
 
-# def start(bot, update):
-#     bot.send_message(chat_id=update.message.chat_id,
-#                      text="I'm a bot, please talk to me!")
 
-# def echo(bot, update):
-#     bot.send_message(chat_id=update.message.chat_id, text=update.message.text)
-
-
-# logging.basicConfig(format='%(asctime)s - %(name)s -%(levelname)s - %(messge)s',
-#                     level=logging.INFO)
-
-# updater = Updater(token='513044196:AAEiWp1XZQ8bft-_HkL-_V-ylEd8P9libuk')
-# dispatcher = updater.dispatcher
-
-# start_handler = CommandHandler('start', start)
-# dispatcher.add_handler(start_handler)
-
-# # echo_handler = MessageHandler(Filters.text, echo)
-# # dispatcher.add_handler(echo_handler)
-
-
-# def caps(bot, update, args):
-#      text_caps = ' '.join(args).upper()
-#      bot.send_message(chat_id=update.message.chat_id, text=text_caps)
-
-# caps_handler = CommandHandler('caps', caps, pass_args=True)
-# dispatcher.add_handler(caps_handler)
-
-
-# from telegram import InlineQueryResultArticle, InputTextMessageContent
-# def inline_caps(bot, update):
-#      query = update.inline_query.query
-#      if not query:
-#          return
-#      results = list()
-#      results.append(
-#          InlineQueryResultArticle(
-#              id=query.upper(),
-#              title='Caps',
-#              input_message_content=InputTextMessageContent(query.upper())
-#          )
-#      )
-#      bot.answer_inline_query(update.inline_query.id, results)
-
-# from telegram.ext import InlineQueryHandler
-# inline_caps_handler = InlineQueryHandler(inline_caps)
-# dispatcher.add_handler(inline_caps_handler)
+# location_keyboard = telegram.KeyboardButton(text="send_location", request_location=True)
+# contact_keyboard = telegram.KeyboardButton(text="send_contact", request_contact=True)
+# custom_keyboard = [[ location_keyboard, contact_keyboard ]]
+# reply_markup = telegram.ReplyKeyboardMarkup(custom_keyboard)
+# bot.send_message(chat_id=355055025, 
+#                  text="Would you mind sharing your location and contact with me?", 
+#                  reply_markup=reply_markup)
 
 
 
