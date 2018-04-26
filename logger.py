ee6c831498e8d97a93a77348f76493c8444c9f50
Andrey Denisyuk <36211097+Skrierz@users.noreply.github.com>
2018-04-26 13:38:59 +0300

    Add files via upload

--- logger.py
+++ logger.py
@@ -3,20 +3,34 @@ import logging
 
 class Logger:
     def __init__(self):
-        self.logger = logging.getLogger('bot')
-        self.logger.setLevel(logging.INFO)
-        self.formatter()
+        self.err_logger = logging.getLogger('errors')
+        self.act_logger = logging.getLogger('actions')
+
+    def errors(self):
+        self.err_logger.setLevel(logging.WARNING)
+        self.file_handler('main_log.log')
+        self.err_logger.addHandler(self.file_handler)
+
+    def actions(self):
+        self.act_logger.setLevel(logging.INFO)
+        self.file_handler('actions.log')
+        self.act_logger.addHandler(self.file_handler)
 
     def formatter(self):
         format = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
         self.formatter = logging.Formatter(format)
 
-    def file_handler(self):
-        file_handler = logging.FileHandler('main_log.log', encoding='utf-8')
-        file_handler.setFormatter(self.formatter)
-        self.logger.addHandler(file_handler)
+    def file_handler(self, arg):
+        self.formatter()
+        self.file_handler = logging.FileHandler(arg, encoding='utf-8')
+        self.file_handler.setFormatter(self.formatter)
 
-    def connection_logs(self, arg):
-        self.file_handler()
-        self.logger.warning(arg)
+    def errors_log(self, arg):
+        if not self.err_logger.hasHandlers():
+            self.errors()
+        self.err_logger.warning(arg)
 
+    def actions_log(self, arg):
+        if not self.act_logger.hasHandlers():
+            self.actions()
+        self.act_logger.info(arg)
